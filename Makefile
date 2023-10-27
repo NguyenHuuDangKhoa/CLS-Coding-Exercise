e10adc983cdafad3f5697d2fae7990e187eb8879
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2023-10-27 06:45:27 -0600

    Update and Refactorize
    Update Makefile with more commands
    Update environment.yml file with new packages
    Refactorize config.yml and main.py

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
+setup-environment:
+	@echo "Install requirements"
+	@conda config --set channel_priority strict
+	@conda env create -f environment.yml
+	@echo "${MAGENTA}Remember to activate your environment ^${RESET}"
+
 setup-package:
 	@echo "Installing src in development mode with ${MAGENTA}pip install -e .${RESET}"
 	@echo "Now you can use the current state of code in src anywhere in this environment"
 	pip install -e .
+
+run-pipeline:
+	@echo "Running main.py"
+	@python main.py
--- main.py
+++ main.py
@@ -7,20 +7,25 @@ from src.features.calculate_mutual_information import mutual_information_multipl
 from src.features.calculate_mutual_information import mutual_info_with_entropy
 from sklearn.metrics import mutual_info_score
 from sklearn.feature_selection import mutual_info_classif
+import structlog
 
+
+logger = structlog.getLogger(__name__)
 
 if __name__ == "__main__":
     # Load configuration from the config.yaml file
     config = load_yaml_config(path="./config.yml")
-    # Read data
-    df = read_data(path=config.get("data").get("path"),
-                chunksize=config.get("data").get("chunksize"),
-                logging=config.get("data").get("logging"))
-    print(df.head())
+    
     # Calculate Discrete Mutual Information
-    if config.get("question") == 1:
-        pass
-    else:
+    if config.get("current_question") == "question_1":
+        df = read_data(path=config.get("question_1").get("data_path"),
+                    chunksize=config.get("question_1").get("chunksize"),
+                    logging=config.get("question_1").get("logging"),)
+        print(df.head())
+        #TODO: Implement more
+    elif config.get("current_question") == "question_2":
+        df = read_data(path=config.get("question_2").get("data_path"),)
+        print(df.head())
         mi_funcs = {
             "mutual_information_binary": mutual_information_binary,
             "mutual_information_multiple_discrete": mutual_information_multiple_discrete,
@@ -29,4 +34,6 @@ if __name__ == "__main__":
             "mutual_info_classif": mutual_info_classif,
         }
         mi_results = compute_mi(data=df,
-                                mi_func=mi_funcs[config.get("mutual_information").get("function")])
+                                mi_func=mi_funcs[config.get("question_2").get("mutual_information_function")])
+    else:
+        logger.info("Error! Please change the current_question parameter in config.yml to either question_1 or question_2")
